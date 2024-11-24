fb8242529b84c60a0c88ddda39f80e15cb3b4b92
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2024-11-24 19:12:23 +0000

    tabs layout

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const TabsLayout = () => {
+  return (
+    <View>
+      <Text>Tabs hLayout</Text>
+    </View>
+  )
+}
+
+export default TabsLayout;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,10 @@
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
-import { StyleSheet, Text, View } from 'react-native';
+
 
 import '../global.css'
 
@@ -15,9 +13,17 @@ import '../global.css'
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+
   const [loaded, error] = useFonts({
     'Poppins-Black': require('../assets/fonts/Poppins-Black.ttf'),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
     'SpaceMono': require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
