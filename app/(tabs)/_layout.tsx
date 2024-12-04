021f022711456a59b6097da94b2b6d3bfab758d9
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2024-12-04 03:15:34 +0000

    onboarding screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,99 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from 'react-native'
+import React from 'react';
+import { Tabs, Redirect } from 'expo-router';
+
+import { icons } from '../../constants'
+
+const TabIcon = ({icon, color, name, focused}: any) => {
+  return(
+    <View className='flex items-center justify-center gap-2 '>
+      <Image source={icon} resizeMode='contain' tintColor={color} className='w-6 h-6'/>
+      <Text className={`${focused ? 'font-psemibold': 'font-pregular'} text-[6px]`} style={{color: color}}>{name}</Text>
+    </View>
+  )
+}
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>Tabs hLayout</Text>
-    </View>
+    <>
+      <Tabs 
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: '#FFA001',
+          tabBarInactiveTintColor: '#CDCDE0',
+          tabBarStyle: {
+            backgroundColor: '#161622',
+            borderTopWidth: 1,
+            borderTopColor: '#232533',
+            height: 74
+          }
+        }}
+      >
+        <Tabs.Screen 
+          name='home'
+          options={{
+            title: 'Home',
+            headerShown: false,
+            tabBarIcon: ({color, focused}: any) => (
+              <TabIcon
+              icon={icons.home}
+              name='Home'
+              focused={focused}
+              color={color}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen 
+          name='bookmark'
+          options={{
+            title: 'Bookmark',
+            headerShown: false,
+            tabBarIcon: ({color, focused}: any) => (
+              <TabIcon
+              icon={icons.bookmark}
+              name='Bookmark'
+              focused={focused}
+              color={color}
+              />
+            )
+          }}
+        />
+
+      <Tabs.Screen 
+          name='create'
+          options={{
+            title: 'Create',
+            headerShown: false,
+            tabBarIcon: ({color, focused}: any) => (
+              <TabIcon
+              icon={icons.plus}
+              name='Create'
+              focused={focused}
+              color={color}
+              />
+            )
+          }}
+        />
+
+        <Tabs.Screen 
+          name='profile'
+          options={{
+            title: 'Profile',
+            headerShown: false,
+            tabBarIcon: ({color, focused}: any) => (
+              <TabIcon
+              icon={icons.profile}
+              name='Profile'
+              focused={focused}
+              color={color}
+              />
+            )
+          }}
+        />
+      </Tabs>
+    </>
   )
 }
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Image, Platform, Text, View } from 'react-native';
-
+import { Image, Platform, ScrollView, Text, View,  } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import { Link } from 'expo-router';
+import { Link } from 'expo-router';;
 
-export default function HomeScreen() {
+import { images } from '@/constants';
+
+export default function App() {
   return (
-    <View  className="flex-1 items-center justify-center bg-white">
-      <Text className='text-xl'>Aora!!!</Text>
-      <StatusBar style='auto'/>
-      <Link href='/profile' style={{color: 'blue'}}>Go to Profile</Link>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className='w-full h-full px-4 justify-center items-center'>
+          <Image source={images.logo} className='w-[130px] h-[84px]' resizeMode='contain'/>
+          <Image source={images.cards} className='max-w-[380px] w-full h-[300px]' resizeMode='contain'/>
+        </View>
+
+        <View className='relative mt-5'>
+          <Text className='rext-3xl text-white font-bold text-center'>
+
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
