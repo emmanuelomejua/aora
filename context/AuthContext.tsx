1816a5072ae29ec3e3a19b7e3ca3a2926536fd1c
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2024-12-24 14:34:15 +0000

    appwite configurations

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,6 +1,34 @@
-import { createContext } from "react";
+import React, { Children, createContext, useContext, useEffect, useState } from "react";
 
 
-const AuthContext = createContext({
-    
-})
+const INITIAL_STATE = {
+    user: null,
+    loading: false,
+    loggedIn: false
+}
+
+const AuthContext = createContext(INITIAL_STATE);
+
+export const useAuthContext = () => useContext(AuthContext);
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+
+    useEffect(() => {
+
+    }, [])
+
+    return(
+        <AuthContext.Provider value={{
+            loggedIn,
+            user,
+            loading,
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
